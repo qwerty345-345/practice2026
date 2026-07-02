535144e145ba2182488c37dcd8b63855cf57778d
qwerty345-345 <mejjsonelli@gmail.com>
2026-07-02 23:41:05 +0600

    task02

---
+++ practice2026/task02/Class1.cs
@@ -0,0 +1,51 @@
+﻿
+using System.Linq;
+
+namespace task02
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Faculty { get; set; }
+        public List<int> Grades { get; set; }
+    }
+
+    public class StudentService
+    {
+        private readonly List<Student> _students;
+
+        public StudentService(List<Student> students)
+        {
+            _students = students;
+        }
+
+        public IEnumerable<Student> GetStudentsByFaculty(string faculty)
+        {
+            return _students.Where(s => s.Faculty == faculty);
+        }
+
+        public IEnumerable<Student> GetStudentsWithMinAverageGrade(double average)
+        {
+            return _students.Where(s => s.Grades.Average() >= average);
+        }
+
+        public IEnumerable<Student> GetStudentsOrderedByName()
+        {
+            return _students.OrderBy(s => s.Name);
+        }
+
+        public ILookup<string, Student> GroupStudentsByFaculty()
+        {
+            return _students.ToLookup(s => s.Faculty);
+        }
+
+        public string GetFacultyWithHighestAverageGrade()
+        {
+            return _students
+                .GroupBy(s => s.Faculty)
+                .OrderByDescending(g => g.Average(s => s.Grades.Average()))
+                .First()
+                .Key;
+        }
+    }
+}
