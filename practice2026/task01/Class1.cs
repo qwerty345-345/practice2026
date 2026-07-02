af90856054437fe3a48f025103b6fa12955f1158
qwerty345-345 <mejjsonelli@gmail.com>
2026-07-02 22:56:41 +0600

    add string extension and tests

---
+++ practice2026/task01/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            string lowerword = input.ToLowerInvariant();
+            string newword = "";
+
+            foreach (char c in lowerword)
+            {
+                if (char.IsLetterOrDigit(c))
+                {
+                    newword += c;
+                }
+            }
+
+            if (newword == new string(newword.Reverse().ToArray()) && newword.Length != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ practice2026/task01tests/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Xunit;
+using task01;
+
+namespace task01tests
+{
+    public class StringExtensionTests
+    {
+        [Fact]
+        public void test1()
+        {
+            Assert.True("А роза упала на лапу Азора".IsPalindrome());
+        }
+
+        [Fact]
+        public void test2()
+        {
+            Assert.False("hello".IsPalindrome());
+        }
+
+        [Fact]
+        public void test3()
+        {
+            Assert.True("казак".IsPalindrome());
+        }
+
+        [Fact]
+        public void test4()
+        {
+            Assert.True("12321".IsPalindrome());
+        }
+
+        [Fact]
+        public void test5()
+        {
+            Assert.False("12345".IsPalindrome());
+        }
+
+        [Fact]
+        public void test6()
+        {
+            Assert.False("".IsPalindrome());
+        }
+    }
+}
