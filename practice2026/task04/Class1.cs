7331d452d76928c9328e904887f3dd7386125ae3
qwerty345-345 <mejjsonelli@gmail.com>
2026-07-03 00:54:43 +0600

    task04

---
+++ practice2026/task04/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace task04
+{
+    public interface ISpaceship
+    {
+        void MoveForward();
+        void Rotate(int angle);
+        void Fire();
+        int Speed { get; }
+        int FirePower { get; }
+    }
+
+    public class Cruiser : ISpaceship
+    {
+        public int Speed => 50;
+        public int FirePower => 100;
+
+        public void MoveForward()
+        {
+            Console.WriteLine("cruiser moves forward slowly");
+        }
+
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"cruiser rotates by {angle} degrees");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine($"cruiser fires photon missile with power {FirePower}");
+        }
+    }
+
+    public class Fighter : ISpaceship
+    {
+        public int Speed => 100;
+        public int FirePower => 50;
+
+        public void MoveForward()
+        {
+            Console.WriteLine("fighter moves forward fast");
+        }
+
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"fighter rotates by {angle} degrees");
+        }
+
+        public void Fire()
+        {
+            Console.WriteLine($"fighter fires photon missile with power {FirePower}");
+        }
+    }
+}
---
+++ practice2026/task04tests/UnitTest1.cs
@@ -0,0 +1,84 @@
+﻿using Xunit;
+using task04;
+
+namespace task04tests
+{
+    public class SpaceshipTests
+    {
+        [Fact]
+        public void Cruiser_ShouldHaveCorrectStats()
+        {
+            ISpaceship cruiser = new Cruiser();
+
+            Assert.Equal(50, cruiser.Speed);
+            Assert.Equal(100, cruiser.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldHaveCorrectStats()
+        {
+            ISpaceship fighter = new Fighter();
+
+            Assert.Equal(100, fighter.Speed);
+            Assert.Equal(50, fighter.FirePower);
+        }
+
+        [Fact]
+        public void Fighter_ShouldBeFasterThanCruiser()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+
+            Assert.True(fighter.Speed > cruiser.Speed);
+        }
+
+        [Fact]
+        public void Cruiser_ShouldHaveMoreFirePowerThanFighter()
+        {
+            var fighter = new Fighter();
+            var cruiser = new Cruiser();
+
+            Assert.True(cruiser.FirePower > fighter.FirePower);
+        }
+
+        [Fact]
+        public void Cruiser_MoveForward_ShouldNotThrow()
+        {
+            var cruiser = new Cruiser();
+
+            var ex = Record.Exception(() => cruiser.MoveForward());
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void Fighter_MoveForward_ShouldNotThrow()
+        {
+            var fighter = new Fighter();
+
+            var ex = Record.Exception(() => fighter.MoveForward());
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void Cruiser_Fire_ShouldNotThrow()
+        {
+            var cruiser = new Cruiser();
+
+            var ex = Record.Exception(() => cruiser.Fire());
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void Fighter_Fire_ShouldNotThrow()
+        {
+            var fighter = new Fighter();
+
+            var ex = Record.Exception(() => fighter.Fire());
+
+            Assert.Null(ex);
+        }
+    }
+}
