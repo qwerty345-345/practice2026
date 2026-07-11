4e9b06203ababfdfbce600afd7dfa7dd395d4ad1
qwerty345-345 <mejjsonelli@gmail.com>
2026-07-12 01:14:51 +0600

    task10

---
+++ practice2026/Plugins/FirstPlugin.cs.cs
@@ -0,0 +1,15 @@
+﻿using PluginCore;
+using System;
+using System.Windows.Input;
+
+namespace Plugins
+{
+    [PluginLoad]
+    public class FirstPlugin : PluginCore.ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Первый плагин выполнен");
+        }
+    }
+}
