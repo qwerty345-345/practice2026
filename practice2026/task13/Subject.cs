15c05a6b68ed7d3aedfdf4dc514ef1ce57c7fb08
qwerty345-345 <mejjsonelli@gmail.com>
2026-07-14 00:07:04 +0600

    task13

---
+++ practice2026/task13/Subject.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json.Serialization;
+
+public class Subject
+{
+    public string Name { get; set; }
+
+    public int Grade { get; set; }
+}
---
+++ practice2026/task13tests/JsonServiceTests.cs
@@ -0,0 +1,87 @@
+﻿using Xunit;
+using System;
+using System.Collections.Generic;
+
+public class JsonServiceTests
+{
+    [Fact]
+    public void Serialize_ReturnsJson()
+    {
+        var service = new JsonService();
+
+        var student = new Student
+        {
+            FirstName = "Ivan",
+            LastName = "Ivanov",
+            BirthDate = new DateTime(2005, 5, 10),
+            Grades = new List<Subject>
+            {
+                new Subject
+                {
+                    Name = "Math",
+                    Grade = 5
+                }
+            }
+        };
+
+        var json = service.Serialize(student);
+
+        Assert.Contains("Ivan", json);
+    }
+
+    [Fact]
+    public void Deserialize_ReturnsStudent()
+    {
+        var service = new JsonService();
+
+        var json =
+@"{
+    ""FirstName"":""Ivan"",
+    ""LastName"":""Ivanov"",
+    ""BirthDate"":""10.05.2005"",
+    ""Grades"":[]
+}";
+
+        var student = service.Deserialize(json);
+
+        Assert.Equal("Ivan", student.FirstName);
+    }
+
+    [Fact]
+    public void SaveAndLoadFile()
+    {
+        var service = new JsonService();
+
+        var student = new Student
+        {
+            FirstName = "Ivan",
+            LastName = "Ivanov",
+            BirthDate = new DateTime(2005, 5, 10),
+            Grades = new List<Subject>()
+        };
+
+        service.SaveToFile(student, "student.json");
+
+        var result = service.LoadFromFile("student.json");
+
+        Assert.Equal("Ivan", result.FirstName);
+    }
+
+    [Fact]
+    public void Deserialize_InvalidStudent_ReturnsNull()
+    {
+        var service = new JsonService();
+
+        var json =
+@"{
+    ""FirstName"":"""",
+    ""LastName"":""Ivanov"",
+    ""BirthDate"":""10.05.2005"",
+    ""Grades"":[]
+}";
+
+        var student = service.Deserialize(json);
+
+        Assert.Null(student);
+    }
+}
